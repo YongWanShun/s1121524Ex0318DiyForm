957c0806cdd77caca18b51b7fdb09e920cfa0886
wanshunyong <s1121524@o365st.pu.edu.tw>
2024-03-25 00:11:26 +0800

    9 button

--- s1121524Ex0318DiyFormLab001/Form1.Designer.cs
+++ s1121524Ex0318DiyFormLab001/Form1.Designer.cs
@@ -31,6 +31,16 @@
             button1 = new Button();
             label1 = new Label();
             tableLayoutPanel1 = new TableLayoutPanel();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -59,11 +69,20 @@
             // 
             tableLayoutPanel1.AutoScroll = true;
             tableLayoutPanel1.AutoSize = true;
-            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetDouble;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
             tableLayoutPanel1.ColumnCount = 3;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 164F));
+            tableLayoutPanel1.Controls.Add(button2, 2, 0);
+            tableLayoutPanel1.Controls.Add(button3, 1, 0);
+            tableLayoutPanel1.Controls.Add(button4, 0, 0);
+            tableLayoutPanel1.Controls.Add(button6, 1, 1);
+            tableLayoutPanel1.Controls.Add(button7, 0, 1);
+            tableLayoutPanel1.Controls.Add(button9, 1, 2);
+            tableLayoutPanel1.Controls.Add(button10, 0, 2);
+            tableLayoutPanel1.Controls.Add(button5, 2, 1);
+            tableLayoutPanel1.Controls.Add(button8, 2, 2);
             tableLayoutPanel1.Location = new Point(22, 17);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 3;
@@ -73,6 +92,90 @@
             tableLayoutPanel1.Size = new Size(492, 431);
             tableLayoutPanel1.TabIndex = 2;
             tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.AutoSize = true;
+            button2.Location = new Point(330, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 141);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Location = new Point(167, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(156, 141);
+            button3.TabIndex = 1;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button4.Location = new Point(4, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(156, 141);
+            button4.TabIndex = 2;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button5.Location = new Point(330, 152);
+            button5.Name = "button5";
+            button5.Size = new Size(158, 141);
+            button5.TabIndex = 3;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button6.Location = new Point(167, 152);
+            button6.Name = "button6";
+            button6.Size = new Size(156, 141);
+            button6.TabIndex = 4;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button7.Location = new Point(4, 152);
+            button7.Name = "button7";
+            button7.Size = new Size(156, 141);
+            button7.TabIndex = 5;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button8.Location = new Point(330, 300);
+            button8.Name = "button8";
+            button8.Size = new Size(158, 127);
+            button8.TabIndex = 6;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button9.Location = new Point(167, 300);
+            button9.Name = "button9";
+            button9.Size = new Size(156, 127);
+            button9.TabIndex = 7;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button10.Location = new Point(4, 300);
+            button10.Name = "button10";
+            button10.Size = new Size(156, 127);
+            button10.TabIndex = 8;
+            button10.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -87,6 +190,8 @@
             Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,5 +201,14 @@
         private Button button1;
         private Label label1;
         private TableLayoutPanel tableLayoutPanel1;
+        public Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
     }
 }
--- s1121524Ex0318DiyFormLab001/Form1.cs
+++ s1121524Ex0318DiyFormLab001/Form1.cs
@@ -22,5 +22,44 @@ namespace s1121524Ex0318DiyFormLab001
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
